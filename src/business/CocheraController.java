f6f279eada0d3f473840f190ce0f9b8e357d81a1
Martin Oliva <moliva@solvd.com>
2024-03-29 23:48:25 -0300

    Cochera controler plus few custom exceptions

--- src/business/CocheraController.java
+++ src/business/CocheraController.java
@@ -1,8 +1,10 @@
 package business;
 
 import java.util.ArrayList;
+
 import data.CocheraData;
 import domain.*;
+import util.CustomExceptions.*;
 
 public class CocheraController {
 	public static ArrayList<Cochera> getAll() {
@@ -12,26 +14,30 @@ public class CocheraController {
 	public static Cochera getOne(int idCochera) {
 		try {
 			return new CocheraData().getOne(idCochera);
-		} catch (Exception e) {
-			throw e;
+		} catch (DatabaseAccessException e) {
+			throw new DatabaseAccessException("Error al intentar obtener una cochera en la base de datos", e);
 		}
 	}
 
 	public static void deleteOne(int idCochera) {
 		try {
 			new CocheraData().deleteOne(idCochera);
-		} catch (Exception e) {
-			throw e;
+		} catch (DatabaseAccessException e) {
+			throw new DatabaseAccessException("Error al intentar eliminar una cochera en la base de datos", e);
 		}
 	}
 
+	@SuppressWarnings("unused")
 	public static void insertOne(Cochera c) {
 		try {
 			Integer capacidad = c.getCapacidad();
-			if (capacidad == null || capacidad < 1) {
-				throw new RuntimeException("La capacidad de la cochera no puede ser nula o menor a 1");
-			} else if (capacidad > 999) {
-				throw new RuntimeException("La capacidad de la cochera no puede ser mayor a 999");
+			if (capacidad == null ) {
+				throw new CapacidadNullException("La capacidad de la cochera no puede ser nula");
+			}else if (capacidad < 1) {
+				throw new CapacidadInvalidaException("La capacidad de la cochera no puede ser menor a 1");
+			}
+			else if (capacidad > 999) {
+				throw new CapacidadInvalidaException("La capacidad de la cochera no puede ser mayor a 999");
 			}
 
 			Integer id = new CocheraData().insertOne(c);
@@ -45,16 +51,16 @@ public class CocheraController {
 				LugarController.insertOne(l);
 			}
 
-		} catch (Exception e) {
-			throw e;
+		} catch (DatabaseAccessException e) {
+			throw new DatabaseAccessException("Error al intentar actualizar el lugar en la cochera en la base de datos", e);
 		}
 	}
 
 	public static void updateOne(Cochera c) {
 		try {
 			new CocheraData().updateOne(c);
-		} catch (Exception e) {
-			throw e;
+		} catch (DatabaseAccessException e) {
+			throw new DatabaseAccessException("Error al intentar actualizar la cochera en la base de datos", e);
 		}
 	}
 }
--- src/util/CustomExceptions.java
+++ src/util/CustomExceptions.java
@@ -27,5 +27,29 @@ public class CustomExceptions {
 			super(message, cause);
 		}
     }
+	
+	public static class CapacidadNullException extends RuntimeException implements Serializable {
+		private static final long serialVersionUID = 537769307935672166L;
+
+		public CapacidadNullException(String message) {
+            super(message);
+        }
+
+		public CapacidadNullException(String message, Throwable cause) {
+			super(message, cause);
+		}
+    }
+	
+	public static class CapacidadInvalidaException extends RuntimeException implements Serializable {
+		private static final long serialVersionUID = 6575455320123354232L;
+
+		public CapacidadInvalidaException(String message) {
+            super(message);
+        }
+
+		public CapacidadInvalidaException(String message, Throwable cause) {
+			super(message, cause);
+		}
+    }
 
 }
